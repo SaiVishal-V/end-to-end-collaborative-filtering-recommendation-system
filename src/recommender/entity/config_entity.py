c8479d781282c70c164b1648153eafdc73a5c934
Vishal <saivishalv4u@gmail.com>
2026-07-20 19:11:48 +0530

    Created dataclass file

---
+++ src/recommender/entity/config_entity.py
@@ -0,0 +1,57 @@
+"""
+What is a Dataclass?
+
+A dataclass is a Python class whose main job is to store data.
+
+Think of it as a container.
+
+For example...
+
+Instead of
+
+student = {
+    "name": "Vishal",
+    "age": 22,
+    "cgpa": 8.87
+}
+
+we write
+
+Student(
+    name="Vishal",
+    age=22,
+    cgpa=8.87
+)
+
+
+"""
+
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: str
+
+    movies_path: str
+    ratings_path: str
+    tags_path: str
+    links_path: str
+
+
+"""
+Why frozen=True?
+
+
+Imagine someone does this:
+
+config.movies_path = "abc.csv"
+
+Oops.
+
+They accidentally changed the configuration.
+
+Configuration should not change while the program is running.
+
+So we freeze it.
+
+"""
